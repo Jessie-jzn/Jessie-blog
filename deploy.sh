c149b024ad5004d94803a558043503c8a846cf9d
jessie <740395858@qq.com>
2022-06-09 15:01:01 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Jessie-jzn/Jessie-blog.git master
+git push -f git@study.github.com:Jessie-jzn/Jessie-blog.git master:gh-pages
 
 cd -
 
